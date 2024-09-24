ffca622f7b6b944df4715e0297fab4c30848c3aa
shanto das <114013679+Shanto-123@users.noreply.github.com>
2024-09-25 03:02:02 +0600

    Add files via upload

---
+++ Lesson-10-if-statement.py
@@ -0,0 +1,10 @@
+is_rainy = False
+is_sunny = True
+if is_rainy:
+    print("Don't need to go out from home")
+elif is_sunny:
+    print('You need to go out with umbrella')
+else:
+    print("Check the weather")
+
+
